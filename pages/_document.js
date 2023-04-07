fa291e942decb033de44c39747d4f2ac935b39e6
pokuong lao <pokuonglao123@gmail.com>
2023-04-07 13:55:02 -0700

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,7 @@ class MyDocument extends Document {
     return (
       <Html lang="en" className="theme-compiled">
         <Head>
-        <script src="https://cdn.jsdelivr.net/npm/three@0.133.2/build/three.min.js"></script>
+        <script defer src="./three.js"></script>
         </Head>
         <body
           className={`antialiased text-lg bg-white dark:bg-gray-900 dark:text-white leading-base`}
