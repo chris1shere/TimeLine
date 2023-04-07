21e442f4b21f4094e0460b6f500fac90db09306b
pokuong lao <pokuonglao123@gmail.com>
2023-04-07 13:23:10 -0700

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function Header({ name, email  }) {
   return (
     <header className="pt-20 pb-12">
-      <script type="module" src="/three.js" async></script>
+      <script type="module" src="./three.js" async></script>
       <div id="canvas-container"></div>
       <p className="text-2xl dark:text-white text-center">
         <Link href="/">
